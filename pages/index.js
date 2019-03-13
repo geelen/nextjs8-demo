73d96b849a021e1948e8c2721236067fdf962951
Glen Maddern <glenmaddern@gmail.com>
2019-03-13 15:51:36 +1100

    changing port to 4000

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ Index.getInitialProps = async function() {
 
   console.log(`Show data fetched! Count: ${data.length}`)
 
-  console.log({data})
+  console.log({shows: data})
 
   return {
     tvShowName,
